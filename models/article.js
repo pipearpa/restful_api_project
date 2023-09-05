9d982d82b459e58c5d6d870ea3da28c52f15ee28
Julian Salazar <jsalazar@sena.edu.co>
2023-09-04 23:27:55 -0500

    First commit

---
+++ models/article.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Article extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Article.belongsTo(model.User)
+      models.User.hasMany(Article)
+
+      Article.belongsToMany(models.Category,{
+        through: "articleCategories",
+        as: "categories"
+      })
+    }
+  }
+  Article.init({
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Article',
+  });
+  return Article;
+};
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express') // Para incluir el Framework
+const app = express(); // Instancia del framework Express
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+// Validamos que no estemos en ambiente de producción
+if(process.env.NODE_ENV != 'production'){
+    // Se carga la configuración archivo .env al process.env
+    require('dotenv').config()
+}
+
+app.set('port', process.env.PORT || 4000)
+
+// Middlewares
+app.use(bodyParser.urlencoded({extended:false}))// Recibir datos formulario sencillos
+app.use(bodyParser.json()) // Para recibir json
+app.use(morgan('combined'))
+
+app.use('/api/v1/users',require('./api/v1/routes/users.routes'))
+
+app.listen(app.get('port'),()=>{
+    console.log(`Server running on localhost:${app.get('port')}`);
+})
